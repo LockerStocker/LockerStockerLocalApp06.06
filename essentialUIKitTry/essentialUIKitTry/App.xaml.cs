fe4e05d68b9d602ea8c7632dde525f1b39a5fe6b
Amir <amirle93@gmail.com>
2021-05-21 15:38:19 +0300

    Add project files.

---
+++ essentialUIKitTry/essentialUIKitTry/App.xaml.cs
@@ -0,0 +1,34 @@
+using essentialUIKitTry.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Montserrat-Bold")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat-Medium")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat-SemiBold")]
+[assembly: ExportFont("UIFontIcons.ttf", Alias = "FontIcons")]
+namespace essentialUIKitTry
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new SimpleLoginPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ essentialUIKitTry/essentialUIKitTry/Views/SimpleLoginCheck.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace essentialUIKitTry.Views
+{
+    /// <summary>
+    /// Page to login with user name and password
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SimpleLoginCheck
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleLoginCheck" /> class.
+        /// </summary>
+        public SimpleLoginCheck()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
