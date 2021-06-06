3b4f95c863c364dc57a8c300aa1b0fccacb74a4f
Amir <amirle93@gmail.com>
2021-06-06 19:47:11 +0300

    06.06 updates

---
+++ essentialUIKitTry/essentialUIKitTry/Views/Locker3ProfilePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace essentialUIKitTry.Views
+{
+    /// <summary>
+    /// Page to show chat profile page
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Locker3ProfilePage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Locker3ProfilePage" /> class.
+        /// </summary>
+        public Locker3ProfilePage()
+        {
+            this.InitializeComponent();
+           
+        }
+        void Tap_try(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new InsideALockerImage());
+        }
+    }
+}
